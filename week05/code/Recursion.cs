de8bd3f35351b819feb003b6cbf1ab7dc385be88
Austin Styer <austin.j.styer@gmail.com>
2024-12-02 20:59:17 -0700

    wk 5 code

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -3,9 +3,6 @@ using System.Collections;
 public static class Recursion
 {
     /// <summary>
-    /// #############
-    /// # Problem 1 #
-    /// #############
     /// Using recursion, find the sum of 1^2 + 2^2 + 3^2 + ... + n^2
     /// and return it.  Remember to both express the solution 
     /// in terms of recursive call on a smaller problem and 
@@ -14,9 +11,16 @@ public static class Recursion
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        // Base case: if n <= 0, return 0
+        if (n <= 0)
+        {
+            return 0;
+        }
+
+        // Recursive case: n^2 + SumSquaresRecursive(n - 1)
+        return n * n + SumSquaresRecursive(n - 1);
     }
+
 
     /// <summary>
     /// #############
@@ -38,88 +42,138 @@ public static class Recursion
     /// and the length of the letters list).
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
+{
+    // Base case: if the current word's length equals the desired size
+    if (word.Length == size)
     {
-        // TODO Start Problem 2
+        results.Add(word);
+        return;
     }
 
-    /// <summary>
-    /// #############
-    /// # Problem 3 #
-    /// #############
-    /// Imagine that there was a staircase with 's' stairs.  
-    /// We want to count how many ways there are to climb 
-    /// the stairs.  If the person could only climb one 
-    /// stair at a time, then the total would be just one.  
-    /// However, if the person could choose to climb either 
-    /// one, two, or three stairs at a time (in any order), 
-    /// then the total possibilities become much more 
-    /// complicated.  If there were just three stairs,
-    /// the possible ways to climb would be four as follows:
-    ///
-    ///     1 step, 1 step, 1 step
-    ///     1 step, 2 step
-    ///     2 step, 1 step
-    ///     3 step
-    ///
-    /// With just one step to go, the ways to get
-    /// to the top of 's' stairs is to either:
-    ///
-    /// - take a single step from the second to last step, 
-    /// - take a double step from the third to last step, 
-    /// - take a triple step from the fourth to last step
-    ///
-    /// We don't need to think about scenarios like taking two 
-    /// single steps from the third to last step because this
-    /// is already part of the first scenario (taking a single
-    /// step from the second to last step).
-    ///
-    /// These final leaps give us a sum:
-    ///
-    /// CountWaysToClimb(s) = CountWaysToClimb(s-1) + 
-    ///                       CountWaysToClimb(s-2) +
-    ///                       CountWaysToClimb(s-3)
-    ///
-    /// To run this function for larger values of 's', you will need
-    /// to update this function to use memoization.  The parameter
-    /// 'remember' has already been added as an input parameter to 
-    /// the function for you to complete this task.
-    /// </summary>
-    public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
+    // Recursive case: iterate through available letters
+    for (int i = 0; i < letters.Length; i++)
     {
-        // Base Cases
-        if (s == 0)
-            return 0;
-        if (s == 1)
-            return 1;
-        if (s == 2)
-            return 2;
-        if (s == 3)
-            return 4;
+        // Choose the current letter and exclude it for the next recursion
+        string newWord = word + letters[i];
+        string remainingLetters = letters.Substring(0, i) + letters.Substring(i + 1);
 
-        // TODO Start Problem 3
+        // Recurse with the updated word and letters
+        PermutationsChoose(results, remainingLetters, size, newWord);
+    }
+}
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
-        return ways;
+  /// <summary>
+/// #############
+/// # Problem 3 #
+/// #############
+/// Imagine that there was a staircase with 's' stairs.  
+/// We want to count how many ways there are to climb 
+/// the stairs.  If the person could only climb one 
+/// stair at a time, then the total would be just one.  
+/// However, if the person could choose to climb either 
+/// one, two, or three stairs at a time (in any order), 
+/// then the total possibilities become much more 
+/// complicated.  If there were just three stairs,
+/// the possible ways to climb would be four as follows:
+///
+///     1 step, 1 step, 1 step
+///     1 step, 2 step
+///     2 step, 1 step
+///     3 step
+///
+/// With just one step to go, the ways to get
+/// to the top of 's' stairs is to either:
+///
+/// - take a single step from the second to last step, 
+/// - take a double step from the third to last step, 
+/// - take a triple step from the fourth to last step
+///
+/// We don't need to think about scenarios like taking two 
+/// single steps from the third to last step because this
+/// is already part of the first scenario (taking a single
+/// step from the second to last step).
+///
+/// These final leaps give us a sum:
+///
+/// CountWaysToClimb(s) = CountWaysToClimb(s-1) + 
+///                       CountWaysToClimb(s-2) +
+///                       CountWaysToClimb(s-3)
+///
+/// To run this function for larger values of 's', you will need
+/// to update this function to use memoization.  The parameter
+/// 'remember' has already been added as an input parameter to 
+/// the function for you to complete this task.
+/// </summary>
+public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
+{
+    // Initialize the memoization dictionary if null
+    if (remember == null)
+    {
+        remember = new Dictionary<int, decimal>();
     }
 
-    /// <summary>
-    /// #############
-    /// # Problem 4 #
-    /// #############
-    /// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is 
-    /// a binary string.  If we introduce a wildcard symbol * into the string, we can say that 
-    /// this is now a pattern for multiple binary strings.  For example, 101*1 could be used 
-    /// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example, 
-    /// 1**1 would result in 4 different binary strings: 1001, 1011, 1101, and 1111.
-    ///	
-    /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
-    /// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
-    /// </summary>
-    public static void WildcardBinary(string pattern, List<string> results)
+    // Check if the result for 's' is already computed
+    if (remember.ContainsKey(s))
     {
-        // TODO Start Problem 4
+        return remember[s];
     }
+
+    // Base Cases
+    if (s == 0)
+        return 0;
+    if (s == 1)
+        return 1;
+    if (s == 2)
+        return 2;
+    if (s == 3)
+        return 4;
+
+    // Solve using recursion with memoization
+    decimal ways = CountWaysToClimb(s - 1, remember) 
+                 + CountWaysToClimb(s - 2, remember) 
+                 + CountWaysToClimb(s - 3, remember);
+
+    // Store the result in the dictionary
+    remember[s] = ways;
+
+    return ways;
+}
+
+
+   /// <summary>
+/// #############
+/// # Problem 4 #
+/// #############
+/// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is 
+/// a binary string.  If we introduce a wildcard symbol * into the string, we can say that 
+/// this is now a pattern for multiple binary strings.  For example, 101*1 could be used 
+/// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example, 
+/// 1**1 would result in 4 different binary strings: 1001, 1011, 1101, and 1111.
+///	
+/// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
+/// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
+/// </summary>
+public static void WildcardBinary(string pattern, List<string> results)
+{
+    // Check if the pattern contains a wildcard '*'
+    int wildcardIndex = pattern.IndexOf('*');
+
+    // Base case: No wildcards left
+    if (wildcardIndex == -1)
+    {
+        results.Add(pattern);
+        return;
+    }
+
+    // Recursive case: Replace the wildcard with '0' and '1' and recurse
+    // Replace '*' with '0'
+    string patternWithZero = pattern.Substring(0, wildcardIndex) + '0' + pattern.Substring(wildcardIndex + 1);
+    WildcardBinary(patternWithZero, results);
+
+    // Replace '*' with '1'
+    string patternWithOne = pattern.Substring(0, wildcardIndex) + '1' + pattern.Substring(wildcardIndex + 1);
+    WildcardBinary(patternWithOne, results);
+}
 
     /// <summary>
     /// Use recursion to insert all paths that start at (0,0) and end at the
